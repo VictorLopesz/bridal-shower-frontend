56f6f3547873edb914459a5be48f4ba58dfa02ab
Victor.Loperz <euvictor_h@hotmail.com>
2024-01-22 15:01:11 -0300

    Criando a sidebar

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,100 @@
+'use client'
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faHeart, faGift, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+
+
+
+const Header = () => {
+
+    const [isClick, setIsClick] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsClick(!isClick);
+    };
+
+    return (
+        <>
+            <nav className="bg-[#8cacc8c6]">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg-px-8">
+                    <div className="flex items-center justify-between h-16">
+                        <div className="flex items-center">
+                            <div className="flex-shrink-0">
+                                <a href="/" className="text-white">Logo</a>
+                            </div>
+                        </div>
+                        <div className="hidden md:block">
+                            <div className="ml-12 flex items-center space-x-1 font-georama font-light text-[14px]">
+                                <a href="/" className="no-underline hover:bg-[#22394a92] text-[#485766]  hover:text-[#F0F2ED] rounded-sm p-2 transition duration-300">
+                                    <FontAwesomeIcon icon={faHouse} width={15} height={15} />
+                                    <span className="ml-1 font-medium">
+                                        Início
+                                    </span>
+                                </a>
+                                <a href="/" className="no-underline hover:bg-[#22394a92] text-[#485766]  hover:text-[#F0F2ED] rounded-sm p-2 transition duration-300">
+                                    <FontAwesomeIcon icon={faHeart} width={15} height={15} />
+                                    <span className="ml-1 font-medium">
+                                        Sobre o casal
+                                    </span>
+                                </a>
+                                <a href="/" className="no-underline hover:bg-[#22394a92] text-[#485766]  hover:text-[#F0F2ED] rounded-sm p-2 transition duration-300">
+                                    <FontAwesomeIcon icon={faGift} width={15} height={15} />
+                                    <span className="ml-1 font-medium">
+                                        Opções de Presentes
+                                    </span>
+                                </a>
+                                <a href="/" className="no-underline hover:bg-[#22394a92] text-[#485766] hover:text-[#F0F2ED] rounded-sm p-2 transition duration-300">
+                                    <FontAwesomeIcon icon={faLocationDot}  width={15} height={15} />
+                                    <span className=" ml-1 font-medium">
+                                        Endereço
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="md:hidden flex items-center">
+                            <button className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                                onClick={toggleNavbar}
+                            >
+                                {isClick ? <AiOutlineClose className="text-[#22394ada] hover:active-[#fff] w-6 h-6" /> : <GiHamburgerMenu className="text-[#22394ada] hover:active-[#fff] w-6 h-6" />}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {isClick && (
+                    <div className="md:hidden">
+                        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                        <a href="/" className="no-underline hover:bg-[#22394a92] block text-[#485766]  hover:text-[#F0F2ED] rounded-sm p-2 transition duration-300">
+                                    <FontAwesomeIcon icon={faHouse} width={15} height={15} />
+                                    <span className="ml-1 font-medium">
+                                        Início
+                                    </span>
+                                </a>
+                                <a href="/" className="no-underline hover:bg-[#22394a92] block text-[#485766]  hover:text-[#F0F2ED] rounded-sm p-2 transition duration-300">
+                                    <FontAwesomeIcon icon={faHeart} width={15} height={15} />
+                                    <span className="ml-1 font-medium">
+                                        Sobre o casal
+                                    </span>
+                                </a>
+                                <a href="/" className="no-underline hover:bg-[#22394a92] block text-[#485766]  hover:text-[#F0F2ED] rounded-sm p-2 transition duration-300">
+                                    <FontAwesomeIcon icon={faGift} width={15} height={15} />
+                                    <span className="ml-1 font-medium">
+                                        Opções de Presentes
+                                    </span>
+                                </a>
+                                <a href="/" className="no-underline hover:bg-[#22394a92] block text-[#485766] hover:text-[#F0F2ED] rounded-sm p-2 transition duration-300">
+                                    <FontAwesomeIcon icon={faLocationDot}  width={15} height={15} />
+                                    <span className=" ml-1 font-medium">
+                                        Endereço
+                                    </span>
+                                </a>
+                        </div>
+                    </div>
+                )}
+            </nav>
+        </>
+    )
+}
+
+export default Header;
