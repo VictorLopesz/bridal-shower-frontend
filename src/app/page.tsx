826bf0948588f7decd86737d7fd352099a55a2de
VictorLopesz <euvictor_h@hotmail.com>
2024-01-25 12:01:24 -0300

    adiciona grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,15 @@ import chaveiro from '../../public/assets/chaveiro.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaw } from '@fortawesome/free-solid-svg-icons';
 import Presentes from "./components/Presentes";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="bg-[url('../../public/assets/fundo.png')] bg-cover w-screen h-screen">
-      <div className="grid grid-cols-1 md:grid-cols-1">
+      <div className="grid grid-cols-1 sm:grid-cols-1 xl:grid-cols-2 lg:grid-cols-2">
         <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-2">
           <div className="mt-12">
-            <img src={gatos.src} alt="gatos" className="mx-auto xl:w-44 w-[150px] " />
+            <Image src={gatos.src} alt="gatos" className="mx-auto xl:w-44 w-[150px] " />
             <span className="uppercase grid-cols-1 grid font-georama font-bold text-[#353931] font-Playfair justify-self-center text-3xl lg:text-4xl xl:text-4xl text-center mt-4">
               Chá de casa nova
               {/* <img src={nomes.src} alt="nomes" className="mx-auto w-[200px] sm:w-[300px]" /> */}
@@ -30,7 +31,7 @@ export default function Home() {
               </ul>
             </div>
             <div className="justify-center flex items-center">
-              <img src={chaveiro.src} alt="chaveiro" width={100} />
+              <Image src={chaveiro.src} alt="chaveiro" width={100} />
             </div>
           </div>
         </div>
