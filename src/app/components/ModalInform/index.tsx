630a3f79129d1ca77b78d97549092ab035079ecc
Victor.Loperz <euvictor_h@hotmail.com>
2024-02-15 12:21:10 -0300

    algumsas modificações

--- src/app/components/ModalInform/index.tsx
+++ src/app/components/ModalInform/index.tsx
@@ -10,6 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import InputMask from 'react-input-mask';
 
 
+
 const ModalInform = () => {
   const [show, setShow] = useState(false);
   const [mensagemErro, setMensagemErro] = useState<string | null>(null);
@@ -50,6 +51,7 @@ const ModalInform = () => {
   }
 
   const handleClose = () => setShow(false);
+  console.log(handleClose);
   const handleShow = () => setShow(true);
 
 
@@ -68,7 +70,7 @@ const ModalInform = () => {
             </div>
           </Modal.Title>
         </Modal.Header>
-        <Modal.Body className="">
+        <Modal.Body>
           <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <label className="uppercase font-semibold text-[10px] -mb-2">Nome e Sobrenome</label>
@@ -78,11 +80,11 @@ const ModalInform = () => {
                     <FontAwesomeIcon icon={faUser} className="text-[#705318]" />
                   </div>
                   <input
-                    type="text"
-                    id="nome"
+                  type='text'
+                    required
+                    id="nome" placeholder="Digite seu nome completo"
                     className="w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 text-sm px-3 py-2.5 rounded-[7px] border"
                     {...register('nome')}
-                    onFocus={handleFocus}
                   />
                 </div>
               </div>
@@ -94,10 +96,14 @@ const ModalInform = () => {
                   <div className="absolute grid w-5 h-5 place-items-center text-blue-gray-500 top-2/4 right-3 -translate-y-2/4">
                     <FontAwesomeIcon icon={faMobileRetro} className="text-[#705318]" />
                   </div>
-                  <InputMask id="phone" mask="(99) 99999-9999" placeholder="(99) 99999-9999"
+                  <InputMask
+                  mask="(99) 99999-9999" maskChar={null} 
+                  type="tel" id="telefone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                    required
+                    placeholder='(00) 00000-0000'
                     className="w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 text-sm px-3 py-2.5 rounded-[7px] border"
                     {...register('telefone')}
-                  ></InputMask>
+                  />
 
                 </div>
               </div>
@@ -116,10 +122,11 @@ const ModalInform = () => {
           <button className="bg-[#727E65] hover:bg-[#34392d] p-2 rounded-md text-white"
             onClick={handleClose}
           >
+            
             Fechar
           </button>
         </Modal.Footer>
-      </Modal>
+      </Modal >
     </>
   );
 }
