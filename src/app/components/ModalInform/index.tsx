9c602fbe80d08e53b2269e559eec05e2f8a09483
Victor.Loperz <euvictor_h@hotmail.com>
2024-02-15 14:49:49 -0300

    tratamento das informações + conserto do modal

--- src/app/components/ModalInform/index.tsx
+++ src/app/components/ModalInform/index.tsx
@@ -8,13 +8,12 @@ import { faMobileRetro } from '@fortawesome/free-solid-svg-icons';
 import { useForm } from 'react-hook-form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import InputMask from 'react-input-mask';
-
-
+import { IoMdAlert } from "react-icons/io";
 
 const ModalInform = () => {
   const [show, setShow] = useState(false);
   const [mensagemErro, setMensagemErro] = useState<string | null>(null);
-  const [tocado, setTocado] = useState<boolean>(false);
+  const [tocado, setTocado] = useState<{ nome: boolean; telefone: boolean }>({ nome: false, telefone: false });
   const { register, handleSubmit, reset } = useForm();
 
   const preencherInput = (nome: any, telefone: any) => {
@@ -22,38 +21,40 @@ const ModalInform = () => {
     const temTelefone = !!telefone;
 
     if (!temNome || !temTelefone) {
-      return "Preencha todos os campos antes de tentar enviar";
+      return "Preencha todos os campos";
     }
     return null;
   };
 
   const onSubmit = (data: any) => {
     const { nome, telefone } = data;
+    console.log(data);
     const erroValidacao = preencherInput(nome, telefone);
 
     if (erroValidacao !== null) {
       setMensagemErro(erroValidacao);
     } else {
-      console.log("Dados enviados:", data);
-      // setShow(false);
+      setShow(false);
+      reset({
+        nome: '',
+        telefone: '',
+      });
     }
-    reset({
-      nome: '',
-      telefone: '',
-    });
-
   }
 
-  const handleFocus = () => {
-    if (tocado) {
+  const handleFocus = (campo: 'nome' | 'telefone'):void => {
+    setTocado((prevState) => ({
+      ...prevState,
+      [campo]: true,
+    }));
+
+    if (mensagemErro !== null) {
       setMensagemErro(null);
     }
   }
 
   const handleClose = () => setShow(false);
-  console.log(handleClose);
   const handleShow = () => setShow(true);
-
 
   return (
     <>
@@ -80,11 +81,12 @@ const ModalInform = () => {
                     <FontAwesomeIcon icon={faUser} className="text-[#705318]" />
                   </div>
                   <input
-                  type='text'
+                    type='text'
                     required
                     id="nome" placeholder="Digite seu nome completo"
                     className="w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 text-sm px-3 py-2.5 rounded-[7px] border"
                     {...register('nome')}
+                    onFocus={() => handleFocus('nome')}
                   />
                 </div>
               </div>
@@ -97,23 +99,28 @@ const ModalInform = () => {
                     <FontAwesomeIcon icon={faMobileRetro} className="text-[#705318]" />
                   </div>
                   <InputMask
-                  mask="(99) 99999-9999" maskChar={null} 
-                  type="tel" id="telefone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                    mask="(99) 99999-9999" maskChar={null}
+                    type="tel" id="telefone"
                     required
                     placeholder='(00) 00000-0000'
                     className="w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 text-sm px-3 py-2.5 rounded-[7px] border"
                     {...register('telefone')}
+                    onFocus={() => handleFocus('telefone')}
                   />
 
                 </div>
               </div>
             </Form.Group>
-            {mensagemErro && <p className="text-red-500" onFocus={handleFocus}>{mensagemErro}</p>}
+            {mensagemErro &&
+              <p className="flex items-center justify-center" onFocus={() => handleFocus}>
+                <IoMdAlert className="animate-pulse text-red-500 w-6 h-6" />
+                <span className="text-red-700 text-xs uppercase">{mensagemErro}</span>
+              </p>}
           </Form>
         </Modal.Body>
         <Modal.Footer>
           <button
-            className="bg-[#917235] hover:bg-[#695327] p-2 rounded-md text-white"
+            className={`bg-[#917235] hover:bg-[#695327] p-2 rounded-md text-white`}
             type="submit"
             onClick={handleSubmit(onSubmit)}
           >
@@ -122,7 +129,7 @@ const ModalInform = () => {
           <button className="bg-[#727E65] hover:bg-[#34392d] p-2 rounded-md text-white"
             onClick={handleClose}
           >
-            
+
             Fechar
           </button>
         </Modal.Footer>
