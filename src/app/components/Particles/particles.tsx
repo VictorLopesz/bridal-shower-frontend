33bda995e7ba6653edb061dba63730d435abc489
VictorLopesz <euvictor_h@hotmail.com>
2024-03-23 20:51:25 -0300

    instalação do react particles

---
+++ src/app/components/Particles/particles.tsx
@@ -0,0 +1,11 @@
+'use client'
+import react from 'react'
+
+
+export default function partciles() {
+    return (
+        <>
+        
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
 'use client'
 import React from "react";
-import gatos from '../../public/assets/gatos.png';
-import nomes from '../../public/assets/nomes.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaw } from '@fortawesome/free-solid-svg-icons';
 import Presentes from "./components/Presentes";
-import Image from "next/image";
-import { FaAngleRight } from "react-icons/fa";
-import { faGift } from '@fortawesome/free-solid-svg-icons';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
+
 
 
 export default function Home() {
   return (
-    <main
-      className="w-full h-full"
-    >
+    <>
       <Presentes />
-    </main >
+    </>
   );
 }
