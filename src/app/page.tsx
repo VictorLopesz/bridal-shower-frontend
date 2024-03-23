c1cf393c9ebc8511913fa5e5d9e2f74f1df898bf
VictorLopesz <euvictor_h@hotmail.com>
2024-03-23 20:58:24 -0300

    adicionando particles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 import React from "react";
 import Presentes from "./components/Presentes";
-
+import Particles from './components/Particles/particles';
 
 
 export default function Home() {
   return (
     <>
       <Presentes />
+      <Particles />
     </>
   );
 }
